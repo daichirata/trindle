b9fe6846425949473301442637d6ea2fe76eb9ab
Daichi Hirata <bunny.hop.md@gmail.com>
2012-10-30 01:54:12 +0900

    add useage

--- trindle.el
+++ trindle.el
@@ -25,9 +25,96 @@
 ;; Unlike package management, only the function of simple management is offered.
 ;; These manage that in which I have installed what from init.el(or any config file).
 
+;;; Credits
+;;
+;; kiwanami/emacs-deffered plays an important feature of this library.
+
+;;; Installation
+;;
+;; $git clone--recursive https://github.com/daic-h/trindle.git
+
 ;;; Useage
 ;;
+;; How to use basic
 ;;
+;;  to require the trindle.
+;;   (require 'trindle)
+;;
+;;  define the package.
+;;   (trindle: packages
+;;    (:type "github":name "daic-h/trindle"))
+;;
+;;  You can install, update, and remove by defining the package.
+;;
+;;   (trindle: install)
+;;   (trindle: install!)
+;;    - delete a file that is not defined in the package, perform the install.
+;;   (trindle: update)
+;;   (trindle: update-all)
+;;   (trindle: remove)
+;;
+;;  Also, these commands can be called interactive.
+;;  call (trindle:initialize), the package file is loaded if defined has been installed.
+
+;;; Customization
+;;
+;; Definition of package
+;;
+;; ・Common
+;;  :init-submodule init a submodule to if you want to clone, default t
+;;  :byte-compile   byte-compiled if you want to clone,       default t
+;;  :load-package   to load when call the trindle:itialize,   default t
+;;  These default values can be changed by trindle:configure
+;;
+;; ・emacswiki
+;;  :type "emacswiki"
+;;  :name - the name of the package emacswiki, required
+;;
+;; · github
+;;  :type "github"
+;;  :name -"username/repository", required
+;;  :branch - branch name when you clone, default "master"
+;;
+;; · git
+;;  :type "git"
+;;  :url - url of the repository, required
+;;  :name - the name of the package, required
+;;  :branch - branch name when you clone, default "master"
+;;
+;; · svn
+;;  :type "svn"
+;;  :url - url of the repository, required
+;;  :name - the name of the package, required
+;;
+;; ・http
+;;  :type "http"
+;;  :url - url of the elis-file, required
+;;  :name - the name of the package, required
+;;
+;; ・http-tar
+;;  :type "http-tar"
+;;  :url - url of the elis-file, required
+;;  :name - the name of the package, required
+;;
+;; Argument by (trindle:configure)
+;;  :dir - directory where the package is installed, default "~/.emacs.d/trindle"
+;;  :smp - number to be executed at the same time in the install and update, default 1
+;;  :init-submodule - it will be the initial value of the package, default t
+;;  :byte-compile   - it will be the initial value of the package, default t
+;;  :load-package   - it will be the initial value of the package, default t
+
+;;; Sample Setting
+;;
+;; (require 'trindle)
+;;
+;; (trindle:packages
+;;  (:type "github" :name "daic-h/initialize")
+;;  (:type "github" :name "emacsmirror/shell-pop")
+;;  (:type "emacswiki" :name "auto-async-byte-compile")
+;;  (:type "http" :name "ruby-mode" :url "http://bugs.ruby-lang.org/projects/ruby-trunk/repository/raw/misc/ruby-mode.el"))
+;;
+;; (trindle:configure :dir "~/.emacs.d/elisp/bundle/" :smp 3)
+;; (trindle:initialize)
 
 ;;; Code:
 (eval-when-compile (require 'cl))
@@ -117,12 +204,12 @@
 (defun* trindle:configure (&key (dir trindle-dir)
                                 (smp trindle-smp)
                                 (byte-compile trindle-byte-compile)
-                                (load-packages trindle-load-packages))
+                                (load-package trindle-load-packages))
   "Initialize the variable dir, smp, byte-compile, load-packages."
   (setq trindle-dir dir
         trindle-smp smp
         trindle-byte-compile byte-compile
-        trindle-load-packages load-packages))
+        trindle-load-packages load-package))
 
 (defun trindle-get-package-dir (package-name)
   "Get install dir by package name."
